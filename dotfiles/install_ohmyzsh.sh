105a60718d5304f9edfc25a98ee527703786e7a1
Troels Laegsgaard <laegsgaardtroels@gmail.com>
2024-10-27 21:07:33 +0100

    Adds .zshrc

--- dotfiles/install_ohmyzsh.sh
+++ dotfiles/install_ohmyzsh.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-plugins=()
-
 # Installs ohmyzsh if it is not already installed.
 if [ ! -d ~/.oh-my-zsh ]
 then
     echo "Install ohmyzsh"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" \
+        --keep-zshrc
 fi
-
